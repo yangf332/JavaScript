1084e15aa49dcaa12328f9f08e921e75dbc550fc
yangf332 <yangf332@gmail.com>
2015-02-15 21:20:07 +0800

    add : 背包问题 - 递归与动态规划方案

--- data_structures_and_algorithms/chapter14/fib.js
+++ data_structures_and_algorithms/chapter14/fib.js
@@ -38,3 +38,88 @@ function iterFib(n)
     }
     return result;
 }
+
+
+
+function lcs(word1, word2)
+{
+    var max = 0;
+    var index = 0;
+    var lcsarr = new Array(word1.length + 1);
+    for (var i = 0; i <= word1.length + 1; i ++) {
+        lcsarr[i] = new Array(word2.length + 1);
+        for (var j = 0; j <= word2.length + 1; j ++) {
+            lcsarr[i][j] = 0;
+        }
+    }
+    for (var i = 0; i <= word1.length; i ++) {
+        for (var j = 0; j <= word2.length; j ++) {
+            if (i == 0 || j == 0) {
+                lcsarr[i][j] = 0;
+            } else {
+                if (word1[i - 1] == word2[j - 1]) {
+                    lcsarr[i][j] = lcsarr[i - 1][j - 1] + 1;
+                } else {
+                    lcsarr[i][j] = 0;
+                }
+            }
+            if (max < lcsarr[i][j]) {
+                max = lcsarr[i][j];
+                index = i;
+            }
+        }
+    }
+
+    var str = '';
+    if (max == 0) {
+        return '';
+    } else {
+        for (var i = index - max; i <= max; i ++) {
+            str += word2[i];
+        }
+        return str;
+    }
+}
+
+// 背包问题
+function max(a, b)
+{
+    return (a > b) ? a : b;
+}
+
+function knapsack(capacity, size, value, n)
+{
+    if (n == 0 || capacity == 0) {
+        return 0;
+    }
+    if (size[n - 1] > capacity) {
+        return knapsack(capacity, size, value, n - 1);
+    } else {
+        return max(value[n - 1] +
+            knapsack(capacity - size[n - 1], size, value, n - 1),
+            knapsack(capacity, size, value, n - 1));
+    }
+}
+
+function dKnapsack(capacity, size, value, n)
+{
+    var K = [];
+    for (var i = 0; i <= capacity + 1; i ++) {
+        K[i] = [];
+    }
+    for (var i = 0; i <= n; i ++) {
+        for (var w = 0; w <= capacity; w ++) {
+            if (i == 0 || w == 0) {
+                K[i][w] = 0;
+            } else if (size[i - 1] <= w) {
+                K[i][w] = max(value[i - 1]) + K[i - 1][w - size[i - 1]],
+                    K[i - 1][w];
+            } else {
+                K[i][w] = K[i - 1][w];
+            }
+            console.log(K[i][w] + ' ');
+        }
+    }
+
+    return K[n][capacity];
+}
